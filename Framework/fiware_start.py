dc9ee2df44522636554f99bb5a21104c58c31ab7
twngbm <twngbm@gmail.com>
2019-05-29 04:36:39 +0800

    init commit: create framework for project.

---
+++ Framework/fiware_start.py
@@ -0,0 +1,19 @@
+import os
+#Check if network exist
+if "fiware_default" not in os.popen("docker network ls").read():
+    print(os.popen("docker network create fiware_default").read())
+    print(os.popen("docker network ls").read())
+
+#Check if mongo-db container exist
+if "mongo-db" in os.popen("docker ps -a").read():
+    db_output=os.popen("docker start mongo-db").read()
+else:
+    db_output=os.popen("docker run -d --name=mongo-db --network=fiware_default --expose=27017 mongo:3.6 --bind_ip_all --smallfiles").read()
+print(db_output)
+#Check if fiware context broker exist
+if "context-broker" in os.popen("docker ps -a").read():
+    fiware_output=os.popen("docker start context-broker").read()
+else:
+    fiware_output=os.popen("docker run -d --name context-broker -h -orion --network=fiware_default -p 1026:1026 context-broker -dbhost mongo-db").read()
+print(fiware_output)
+print(os.popen("docker ps -a").read())
